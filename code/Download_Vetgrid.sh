fe2a7c32a702fca3f242e2e10f713c761db951a6
bosco <boscogracia97@gmail.com>
2023-07-06 17:45:12 +0200

    Modified the workflow to work with ENA metadata

--- code/Download_Vetgrid.sh
+++ code/Download_Vetgrid.sh
@@ -7,21 +7,33 @@
 ### VARIABLES
 RAW_READS="/Volumes/Temp/DrosEU"
 WORKDIR="/Volumes/Data/Dropbox (PopGen)/Bosco/PhD_Dropbox/DrosEU-microbiome/data"
-READSDIR="/Volumes/Data/Dropbox (PopGen)/Bosco/PhD_Dropbox/Ancestral_microbiome/data/R1_mapping/00_READS"
-METADATA=""$WORKDIR"/metadata.csv"
-REFERENCE="/Volumes/Data/Dropbox (PopGen)/Bosco/PhD_Dropbox/Ancestral_microbiome/data/poolseq_reads/dsimM252v1.2+microbiome.fa"
 
 ### COMMANDS
+
+# We check if the folder where the raw reads will be downloaded exists
 if [[ ! -d "$RAW_READS" ]]
-then  
+then
   mkdir -p \
     "$RAW_READS"
 fi
 
-# With this command we can download the metadata from the BioProjects
-esearch -db sra -query 'PRJNA388788[bioproject]' | efetch -format runinfo > "$WORKDIR"/NCBI_PRJNA388788.csv
+# We check if we have the ENA files needed to download the reads. 
+# These files have to be downloaded manually from the ENA webpage.
+# I select the fields: "run_accession", "experimental_alias", "fastq_ftp" and "library_name".
+if [[ ! -f "$WORKDIR"/ENA_* ]]
+then
+  echo "You need at least one ENA file if you want to download reads."
+  exit
+fi
 
-esearch -db sra -query 'PRJNA308584[bioproject]' | efetch -format runinfo > "$WORKDIR"/NCBI_PRJNA308584.csv
+# We create a txt file with the ftp paths. The forth column contains the ftp paths.
+cat "$WORKDIR"/ENA_* | cut -f4 | tr ";" "\n" | grep -v "fastq_ftp" > "$WORKDIR"/ftp.txt
 
+# We download all the files into "RAW_READS"
+wget -P "$RAW_READS" -i "$WORKDIR"/ftp.txt
 
+# Using the ENA information we rename the files to make them match the library name
+for i in $(cat "$WORKDIR"/ENA_* | cut -f1,2 | tr "\t" ",")
+do  rename "s/$(echo "${i}" | cut -f1 -d ",")\./$(echo "${i}" | cut -f2 -d ",")\./" "$RAW_READS"/*
+done
 
